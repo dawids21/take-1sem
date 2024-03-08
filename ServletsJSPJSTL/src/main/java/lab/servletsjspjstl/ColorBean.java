f944b671bd9359d11252eafa644b937a169b72f3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2024-03-08 12:26:41 +0100

    servlets

---
+++ ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/ColorBean.java
@@ -0,0 +1,31 @@
+package lab.servletsjspjstl;
+
+public class ColorBean {
+    private String foregroundColor;
+    private String backgroundColor;
+    private boolean showBorders;
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public boolean isShowBorders() {
+        return showBorders;
+    }
+
+    public void setShowBorders(boolean showBorders) {
+        this.showBorders = showBorders;
+    }
+}
