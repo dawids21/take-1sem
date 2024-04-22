df2d3c8fa8bc4d555ea21c136675e70843df319f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2024-04-22 19:31:48 +0200

    MvcNews

---
+++ MvcNews/MvcNews/Data/NewsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNews.Models;
+using System.Collections.Generic;
+
+namespace MvcNews.Data
+{
+	public class NewsDbContext : DbContext
+	{
+		public NewsDbContext(DbContextOptions<NewsDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<NewsItem> News { get; set; } = null!;
+	}
+}
